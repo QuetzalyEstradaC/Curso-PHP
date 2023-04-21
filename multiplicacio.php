b96a7f0e5c9fb18adccc0c3fbffddf9c5d8faf65
Zaly - Quetzaly <107092686+QuetzalyEstradaC@users.noreply.github.com>
2023-04-21 00:15:39 -0600

    Create multiplicacio.php

---
+++ multiplicacio.php
@@ -0,0 +1,9 @@
+//generar la tabla de multiplicaciones utilizando el ciclo while
+$numero = 7;
+$i = 1;
+do{
+    echo $numero. " x ". $i. " = ".($i * $numero). "</br>";
+    $i ++;
+}
+while($i <= 60);
+?>
